5be0476a0a14344171d84f2a1ac27be99eee5cc0
Akhil <Akhil@MacBook-Pro.local>
2017-10-12 18:19:49 -0700

    PDF Parsing

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import os
+import PyPDF2
 from flask import Flask, request, redirect, url_for
 from werkzeug.utils import secure_filename
 
@@ -9,9 +10,19 @@ ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg'])
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
+def read_data(filename):
+    pdfFileObj = open('files/' + filename, 'rb')
+    pdfReader = PyPDF2.PdfFileReader(pdfFileObj)
+    number_of_pages = pdfReader.getNumPages()
+    for page_number in range(number_of_pages): 
+        page = pdfReader.getPage(page_number).extractText().split(" ")  
+        
+
 def allowed_file(filename):
     return '.' in filename and \
            filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+
 
 @app.route('/', methods=['GET', 'POST'])
 def upload_file():
@@ -42,6 +53,7 @@ from flask import send_from_directory
 
 @app.route('/uploads/<filename>')
 def uploaded_file(filename):
+    read_data(filename)
     return send_from_directory(app.config['UPLOAD_FOLDER'],
                                filename)
 
